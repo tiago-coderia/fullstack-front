e181343ed5fe14521309177f2f5e15d90dca71b3
tiago-coderia <asimplekreative@gmail.com>
2025-10-15 07:10:05 -0300

    Ajustado endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   let error: string | null = null;
 
   try {
-    const res = await fetch("https://api.asimplekreative.com.br/", {
+    const res = await fetch("https://api.asimplekreative.com.br/api/data", {
       cache: "no-store", // Garante que os dados sejam sempre buscados no servidor
     });
 
